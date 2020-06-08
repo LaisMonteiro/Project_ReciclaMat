15c4fa054f589e06c37c759a053df4079ff1f5f1
karenrogersn <56484176+karenrogersn@users.noreply.github.com>
2020-06-08 22:54:14 +0100

    post add form

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import PostAdd from './views/Post/postAdd';
 import EditProfile from './views/Profile/editProfile';
 import Profile from './views/Profile/profile';
 import Search from './views/Search/index';
-import SimpleMap from './components/SimpleMap';
+// import SimpleMap from './components/SimpleMap';
 
 function App() {
   return (
@@ -29,5 +29,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/views/LandingPage/index.jsx
+++ src/views/LandingPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-// import tree from '../../../public/images/tree.svg';
+
 
 const LandingPage = () => {
   return (
