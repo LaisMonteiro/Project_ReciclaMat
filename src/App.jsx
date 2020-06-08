11e50337ca7fb48d379a48ad9e50a529b79727bd
karenrogersn <56484176+karenrogersn@users.noreply.github.com>
2020-06-08 13:16:30 +0100

    google maps api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import LandingPage from './views/LandingPage';
+import SimpleMap from './components/SimpleMap';
 
-function App() {
-  return (
-  <div className="App">
-    <LandingPage />
-  </div>
-  )
+//importing google maps API
+
+// import GoogleMapReact from 'google-map-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LandingPage />
+        <SimpleMap />
+      </div>
+    );
+  }
 }
+
 
 export default App;
