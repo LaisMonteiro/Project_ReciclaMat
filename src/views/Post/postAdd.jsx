5a5b13eb255e7a31d31aacde5d7733007ae57ad3
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-08 12:47:35 +0100

    routes

--- src/views/Post/postAdd.jsx
+++ src/views/Post/postAdd.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.scss';
 
-const EditProfile = () => {
+const PostAdd = () => {
   return (
     <div>
-      <h1>Página EditProfile</h1>
+      <h1>Página de Post Add</h1>
     </div>
   );
 };
-export default EditProfile;
+export default PostAdd;
