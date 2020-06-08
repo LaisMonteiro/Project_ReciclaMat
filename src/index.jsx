567ff784b054a39841ea51c9ae21049c9be1f695
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-08 10:43:36 +0100

    change

--- src/index.jsx
+++ src/index.jsx
@@ -9,15 +9,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-<<<<<<< HEAD
     <App />
-=======
-<<<<<<< HEAD
-      <App />
-=======
-    <App />
->>>>>>> 8e2e76e5d86f4d267bcb1319d49a9f094a839b2f
->>>>>>> 713ed7751a83246cdd66f8435cc54aa573489fb6
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
