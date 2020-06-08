b7c0f54bd3d809b95222cea52ebbfab63756c720
cristianekoshino <cristianekoshino@gmail.com>
2020-06-08 21:57:52 +0100

    sign-up form connected to backend

--- src/views/Authentication/sign-in.jsx
+++ src/views/Authentication/sign-in.jsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sign.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 const AuthenticationSignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    const { email, password } = formData;
+    const data = new FormData();
+
+    data.append('email', email);
+    data.append('password', password);
+
+    axios
+      .post('http://localhost:3010/api/authentication/sign-in', data)
+      .then((user) =>
+       console.log('logar', user))  
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className="signs">
-      <Form>
+      <Form onSubmit={handleSubmitForm}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="name"
+            id="name"
+            onChange={handleInputChange}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+            onChange={handleInputChange}
+          />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
+        {/*         <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
         <Button id="button" size="md" block variant="primary" type="submit">
           Sign in
         </Button>
--- src/views/Authentication/sign-up.jsx
+++ src/views/Authentication/sign-up.jsx
@@ -1,26 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sign.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+import Dropzone from '../../components/DropZone';
 
 const AuthenticationSignUp = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const [selectedFile, setSelectedFile] = useState();
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    const { name, email, password } = formData;
+    const data = new FormData();
+    data.append('name', name);
+    data.append('email', email);
+    data.append('password', password);
+    
+    if (selectedFile) {
+      data.append('avatar', selectedFile);
+    }
+
+    axios
+      .post('http://localhost:3010/api/users', data)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div>
-      <Form>
-        <Form.Group controlId="formGroupEmail">
-          <Form.Control type="text" placeholder="Normal text" />
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Form>
-      <Form>
+      <Form onSubmit={handleSubmitForm}>
         <Form.Group>
-          <Form.File id="exampleFormControlFile1" label="Example file input" />
+          <Form.Control
+            type="text"
+            placeholder="Normal text"
+            name="name"
+            id="name"
+            onChange={handleInputChange}
+          />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            id="email"
+            onChange={handleInputChange}
+          />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+            onChange={handleInputChange}
+          />
         </Form.Group>
+
+        <Form.Group>
+          <Dropzone onFileUploaded={setSelectedFile} />
+        </Form.Group>
+
+        <Button id="button" size="md" block variant="primary" type="submit">
+          Sign up
+        </Button>
       </Form>
-      <Button id="button" size="md" block variant="primary" type="submit">
-        Sign up
-      </Button>
       <small>
         Already a member?{' '}
         <a id="links" href="/signin">
