b1987faf35ae5eb22482329a87fdbed679d8c625
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-08 16:39:44 +0100

    Sign in view

--- src/views/Authentication/sign-up.jsx
+++ src/views/Authentication/sign-up.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './sign.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
 
 const AuthenticationSignUp = () => {
   return (
     <div>
-      <h1>Página de sign-up</h1>
+      <Form>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Control type="text" placeholder="Normal text" />
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form>
+      <Form>
+        <Form.Group>
+          <Form.File id="exampleFormControlFile1" label="Example file input" />
+        </Form.Group>
+      </Form>
+      <Button id="button" size="md" block variant="primary" type="submit">
+        Sign up
+      </Button>
+      <small>
+        Already a member?{' '}
+        <a id="links" href="/signin">
+          Sign in!
+        </a>
+      </small>
     </div>
   );
 };
