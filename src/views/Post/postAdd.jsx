ad91693822135cfc2ff8d08dc4d9c1541d89a273
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-09 15:36:41 +0100

    fixed bootstrap in postAdd

--- src/views/Post/postAdd.jsx
+++ src/views/Post/postAdd.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, Card, ListGroup, ListGroupItem, CardGroup } from 'react-bootstrap';
+import { Card, CardGroup } from 'react-bootstrap';
 
 import './index';
 
