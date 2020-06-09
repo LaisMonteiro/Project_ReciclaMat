e135486f2770459420b44c8e6e209388e23f1a4e
Lais Monteiro <lmonteiro189@gmail.com>
2020-06-09 16:18:45 +0100

    profile

--- src/views/Profile/profile.jsx
+++ src/views/Profile/profile.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from 'react-bootstrap';
@@ -6,16 +7,42 @@ import { Card } from 'react-bootstrap';
 //CRIAR CONDICIONAL, SE OWNER, VER OPCAO DE EDITAR PERFIL.
 
 const Profile = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  const fetchData = () => {
+    setLoading(true);
+    axios
+      .get('')
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => fetchData(), []);
+
   return (
     <div>
-      <div className="box">
-        <h1>IMG</h1>
-        <div>
-          <h5>Profile's Name</h5>
-          <small>Contact:</small>
-          <button className="edit">Edit</button>
-        </div>
-      </div>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        data.map((user) => {
+          return (
+            <div className="box">
+              <img src={user.avatar} alt="" />
+              <div>
+                <h5>{user.name}</h5>
+                <small>Contact: {user.email}</small>
+                <button className="edit">Edit</button>
+              </div>
+            </div>
+          );
+        })
+      )}
+
       <div>
         <p>My items:</p>
         <Card style={{ width: '18rem' }}>
