4f5a2ac575fc06b674f4247195a850d1fc95e1ab
cristianekoshino <cristianekoshino@gmail.com>
2020-06-10 15:27:30 +0100

    for merge

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -13,16 +13,16 @@ const NavBar = () => {
             RECICLAMAT
           </Navbar.Brand>
         </div>
-        <div>
+        <div className="menu-dropdown">
           {/* configurar o botao de ENTRAR (abaixo) quando o Authentication estiver pronto */}
           {/* <Link to="/signIn">Entrar</Link> */}
-          <Navbar expand="lg">
+          <Navbar expand="lg" className="menu-container">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <NavDropdown title="Menu" id="basic-nav-dropdown">
                 <NavDropdown.Item href="/posts">Home</NavDropdown.Item>
                 <NavDropdown.Item href="/profile/:id">Profile</NavDropdown.Item>
-                <NavDropdown.Item href="/post/add">Add Materials</NavDropdown.Item>
+                <NavDropdown.Item href="/post/add">Create Post</NavDropdown.Item>
                 <NavDropdown.Item href="/search">Search Materials</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="/">Sign out</NavDropdown.Item>
